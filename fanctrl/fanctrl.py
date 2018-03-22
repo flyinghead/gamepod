e866aa1bf152666befdec273fd5de7c80fe9f2d7
Flyinghead <raphael.jean@gmail.com>
2018-03-22 23:37:33 +0100

    change PI(D) params and set temp

--- fanctrl/fanctrl.py
+++ fanctrl/fanctrl.py
@@ -6,12 +6,12 @@ import signal
 import sys
 import RPi.GPIO as GPIO
 
-desiredTemp = 70 # The maximum temperature in Celsius after which we trigger the fan
+desiredTemp = 75 # The maximum temperature in Celsius after which we trigger the fan
 		 # Max temp for BCM2837 is 85 C. Pi thermal throttle probably around 82 C
 
 # PI(D) params
-pTemp=20
-iTemp=1.2
+pTemp=15
+iTemp=0.6
 
 fanSpeed=100
 sum=0
@@ -32,14 +32,14 @@ def handleFan():
     fanSpeed=pDiff +iDiff
     if fanSpeed>100:
         fanSpeed=100
-    if fanSpeed<20:
+    if fanSpeed<0:
         fanSpeed=0
     if sum>100:
         sum=100
     if sum<-50:
         sum=-50
     #print("actualTemp %4.2f TempDiff %4.2f pDiff %4.2f iDiff %4.2f fanSpeed %5d" % (actualTemp,diff,pDiff,iDiff,fanSpeed))
-    Pwm.ChangeDutyCycle(fanSpeed)
+    Pwm.ChangeDutyCycle(int(fanSpeed))
     return()
 
 def fanOFF():
