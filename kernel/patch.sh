ec8bb1fc877dbf8a354837f4c0a1183baf5c5bde
Flyinghead <raphael.jean@gmail.com>
2018-04-19 17:23:30 +0000

    Compile PWM fan and backlight driver modules on Stretch

--- kernel/patch.sh
+++ kernel/patch.sh
@@ -2,27 +2,42 @@
 
 set -e
 
-sudo apt-get install bc
-wget https://github.com/raspberrypi/linux/archive/be2540e540f5442d7b372208787fb64100af0c54.zip
-unzip be2540e540f5442d7b372208787fb64100af0c54.zip
-cd linux-be2540e540f5442d7b372208787fb64100af0c54
+function make_and_install_kernel() {
+	make -j4 LOCALVERSION=+ zImage
+
+	echo Installing kernel image in /boot with sudo
+	sudo cp arch/arm/boot/zImage /boot/kernel7.img
+}
+
+sudo apt-get install build-essential libncurses5-dev bc
+
+FIRMWARE_HASH=$(zgrep "* firmware as of" /usr/share/doc/raspberrypi-bootloader/changelog.Debian.gz | head -1 | awk '{ print $5 }')
+# get git hash for this kernel
+KERNEL_HASH=$(wget https://raw.github.com/raspberrypi/firmware/$FIRMWARE_HASH/extra/git_hash -O -)
+
+wget https://github.com/raspberrypi/linux/archive/${KERNEL_HASH}.zip
+unzip -o ${KERNEL_HASH}.zip
+cd linux-${KERNEL_HASH}
+
 patch -p1 <../gpio-poweroff.patch
 KERNEL=kernel7
 make bcm2709_defconfig
-#CONFIG_SENSORS_PWM_FAN=m CONFIG_BACKLIGHT_PWM=y
 
-grep -v '^CONFIG_SENSORS_PWM_FAN=' .config > .config.tmp
-echo CONFIG_SENSORS_PWM_FAN=m >> .config.tmp
-mv .config.tmp .config
+if lsb_release -c | grep -q jessie ; then
+	make_and_install_kernel
+else
+	grep -v '^CONFIG_SENSORS_PWM_FAN=' .config > .config.tmp
+	echo CONFIG_SENSORS_PWM_FAN=m >> .config.tmp
+	mv .config.tmp .config
 
-grep -v '^CONFIG_BACKLIGHT_PWM=' .config > .config.tmp
-echo CONFIG_BACKLIGHT_PWM=m >> .config.tmp
-mv .config.tmp .config
+	grep -v '^CONFIG_BACKLIGHT_PWM=' .config > .config.tmp
+	echo CONFIG_BACKLIGHT_PWM=m >> .config.tmp
+	mv .config.tmp .config
 
-make -j4 LOCALVERSION=+ zImage modules
+	make_and_install_kernel
 
-echo Installing kernel image in /boot as sudo
-sudo cp arch/arm/boot/zImage /boot/kernel7.img
+	make -j4 LOCALVERSION=+ modules
 
-echo Installing kernel modules in /lib as sudo
-sudo make modules_install
+	echo Installing kernel modules in /lib with sudo
+	sudo make modules_install
+fi
